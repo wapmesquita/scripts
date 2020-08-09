74b5730986b5062d945866bbbe5dac74c6c273c1
Walter M <wapmesquita@gmail.com>
2020-08-09 10:55:46 -0300

    Updating

--- dev_env/chrome.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -xe
-
-cd /tmp
-
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-
-dpkg -i google-chrome-stable_current_amd64.deb
-
-apt-get install -f -y
--- dev_env/dbeaver.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -xe
-
-cd /tmp
-
-wget https://dbeaver.jkiss.org/files/dbeaver-ce_latest_amd64.deb
-
-apt-get install dbeaver-ce_latest_amd64.deb
